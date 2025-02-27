c0482cb2e7c9ae2c3a3dde60ee4af06b9f021734
YayeFatou07 <kaneyafatou20@gmail.com>
2025-02-27 12:38:32 +0000

    Partie Connexion et Historique

--- src/Pages/Admin/Inscription/index.jsx
+++ src/Pages/Admin/Inscription/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Formulaire from './Formulaire'; // Remplacez par le bon chemin relatif si nécessaire
+import imagePath from '../../../assets/images/other-images/Administration_9.png'; // Importer l'image
 
 function Inscrire() {
   const divStyle = {
-    backgroundImage: 'url("../../../../assets/images/other-images/Administration_9.png")', // Chemin relatif vers l'image
+    backgroundImage: `url(${imagePath})`, // Utilisation de l'image importée
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     height: '100vh', // Hauteur de l'écran
@@ -14,7 +16,7 @@ function Inscrire() {
 
   return (
     <div style={divStyle}>
-      <h1>test</h1>
+      <Formulaire /> {/* Utilisation du composant Formulaire ici */}
     </div>
   );
 }
