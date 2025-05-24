992ca1f94dd16dc5151bca4bc667870d798decc4
ndiawar <dndiawar20@gmail.com>
2025-05-24 04:15:50 +0000

    Je met à jours mes environnements

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ const queryClient = new QueryClient({
 
 // Configuration d'axios
 axios.defaults.baseURL = process.env.REACT_APP_API_URL || "https://jokkohealthback.onrender.com/api/";
+console.log(axios.defaults.baseURL);
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.withCredentials = true;
